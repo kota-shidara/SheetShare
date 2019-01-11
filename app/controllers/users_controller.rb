fe5e5464b764e4bd68d7bca4165e4274ace9c78f
kota-shidara <3t.u1.kota@gmail.com>
2019-01-11 15:52:08 +0900

    create user_model, user-new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("#")
+    else
+      render("users/new")
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => "home#top"
+
+  get "users/new" => "users#new"
+  post "users/create" => "users#create"
+
+  get "/" => "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
