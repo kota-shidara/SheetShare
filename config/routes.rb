6516cf3e0f67c8522e37827a90e02b2ae21e7bfd
kota-shidara <3t.u1.kota@gmail.com>
2019-01-13 18:19:52 +0900

    change routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Rails.application.routes.draw do
 
-#   Prefix Verb   URI Pattern               Controller#Action
-#     login GET    /login(.:format)          users#login_form
-#           POST   /login(.:format)          users#login
-#    logout POST   /logout(.:format)         users#logout
-#     users POST   /users(.:format)          users#create
-#  new_user GET    /users/new(.:format)      users#new
-# edit_user GET    /users/:id/edit(.:format) users#edit
-#      user GET    /users/:id(.:format)      users#show
-#           PATCH  /users/:id(.:format)      users#update
-#           PUT    /users/:id(.:format)      users#update
-#           DELETE /users/:id(.:format)      users#destroy
-#      root GET    /                         home#top
 
-  get "/login" => "users#login_form"
-  post "/login" => "users#login"
-  post "/logout" => "users#logout"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#delete"
   resources :users, only: [:show, :new, :create, :edit, :update]
 
   root "home#top"
