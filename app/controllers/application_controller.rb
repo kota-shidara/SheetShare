c1c5195f9f764c3e904193e0341e9e2044a2c7f6
kota-shidara <3t.u1.kota@gmail.com>
2019-01-14 11:05:26 +0900

    change set_current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_current_user
-  
+
   include SessionsHelper
 
   def set_current_user
-    @current_user = current_user
+    current_user
   end
 
   # # ログインしていないユーザーのアクセスを拒否
