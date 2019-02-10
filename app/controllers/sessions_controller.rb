eca0a6550945037f7042e7dfa644aeec378a898b
kota-shidara <3t.u1.kota@gmail.com>
2019-02-10 17:12:52 +0900

    add email.downcase

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ before_action :forbid_logged_in_user, {only: [:new, :create]}
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       flash[:notice] = "ログインしました"
