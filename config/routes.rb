aa5d3c9bdce2e9d5153bbfc004275caa2cf69163
kota-shidara <3t.u1.kota@gmail.com>
2019-02-18 00:00:37 +0900

    change routing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#delete"
   resources :users, only: [:show, :new, :create, :edit, :update]
-
+  resources :sales, only: [:show, :new, :create, :edit, :update, :destroy]
+  get "/sales/:id/step2" => "sales#step2"
+  get "/sales/:id/check" => "sales#check"
+  post "sales/:id/confirm" => "sales#confirm"
+  get "/sales/:id/complete" => "sales#complete"
   root "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
